1f9941f89adbf23631c77149f282c5875d8e67d6
Your NameEmmanuel <agbavweisaac@gmail.com>
2023-01-12 22:08:04 +0100

    Fix deleted contract bin references

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,6 +1,4 @@
-import * as adminCtc from './build/admin.main.mjs'
-import * as loanCtc from './build/loan.main.mjs'
 import * as algo_nnt from './build/loan_algo_nnt.main.mjs'
 import * as nnt_algo from './build/loan_nnt_algo.main.mjs'
 import * as nnt_nnt from './build/loan_nnt_nnt.main.mjs'
-export { adminCtc, loanCtc, algo_nnt, nnt_algo, nnt_nnt }
+export { algo_nnt, nnt_algo, nnt_nnt }
