34763b48a6a0e1dd9bc5954cae426089e5766045
Your NameEmmanuel <agbavweisaac@gmail.com>
2023-01-21 15:27:13 +0100

    Update unused import cleanup

--- src/context/ReachContext.js
+++ src/context/ReachContext.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
 	loadStdlib,
 	ALGO_MakeAlgoSignerConnect as MakeAlgoSignerConnect,
